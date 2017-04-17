b7fb4aa6ff495cee388ddee9e9dfc9a93c42522c
celinec <danceagain@hotmail.fr>
2017-04-17 17:04:05 +0200

    Fonctions fermat et sq&mul théoriques

--- fonction.c
+++ fonction.c
@@ -2,22 +2,10 @@
 #include <gmp.h>
 
 
-void fermat(mpz_t n, mpz_t k)
-{
-	
-	
-}
+
 
 void miller_rabin(mpz_t n, mpz_t k)
 {
 
 	
-}
-
-void square_multiply(mpz_t a, mpz_t n, mpz_t h)
-{
-	mpz_t result;
-	mpz_init(result);
-	mpz_set(result, a);
-	mpz_clear(result);
 }
