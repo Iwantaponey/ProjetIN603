499a1802c78020829d8a2751bc98bf91838fce17
celinec <danceagain@hotmail.fr>
2017-04-23 00:13:35 +0200

    Miller-Rabin fonctionne parfaitement !

--- fonction.c
+++ fonction.c
@@ -1,73 +1,29 @@
-#include <stdio.h>
 #include <gmp.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include "fonction.h"
-
-expo binaire(mpz_t h)
-{
-	int i=0;
-	mpz_t x,r,d,u,q,z; 
-	char * str = malloc(2048*sizeof(char)); 
-	mpz_init(r); mpz_init(q);
-	mpz_init_set_str(d,"2",10);
-	mpz_init_set_str(u,"1",10);
-	mpz_init_set_str(z,"0",10);
-	mpz_init_set(x,h);
-	
-	
-	while(mpz_cmp(x,z) && (i < 2048))	
-	{	
-		mpz_fdiv_r(r,x,d);
-		mpz_fdiv_q(q,x,d);
-		if(!mpz_cmp(r,u))
-			str[i]='1';
-		else 
-			str[i]='0';
-		mpz_set(x,q);
-		i++;
-	}
-	printf("str = %s \n", str);
-	printf("taille de str= %d \n", i);
-	mpz_clear(d);
-	mpz_clear(q);
-	mpz_clear(u);
-	mpz_clear(x);
-	mpz_clear(r);
-	expo e = {str, strlen(str)};
-	return e;
-}
-
 
 // La variable result sert à stocker le résultat de l'exponentiation rapide pour l'utiliser dans la fonction fermat
 // a est l'entier qui porte l'exposant h qui est aussi un entier, n est le modulo de a
 void square_multiply(mpz_t a, mpz_t n, mpz_t h, mpz_t result) 
 {
-	expo e;
-	mpz_set(result, a); // Mettre la valeur de a dans result
+	mpz_set(result, a);
 	
-	e = binaire(h);
-	printf("Dans square_multiply, str = %s \n", e.str);	 
+	size_t taille = mpz_sizeinbase(h, 2); // Donner le nombre de bits de h 
 	
-	int i ;
-	for (i = e.t-2; i >= 0; i--)
+	int i;
+	for (i = taille-2; i >= 0; i--)
 	{
-		mpz_mul(result, result, result); // Square
-		mpz_mod(result, result, n);
-		gmp_printf("Carré de result : %Zd\n", result);
-		printf("e.str[%d]= %c \n", i, e.str[i]);
-		gmp_printf("Dans la boucle for, i = %d, result = %Zd , e.str[%d]=%c\n", i, result, i, e.str[i]);
-		if(e.str[i]=='1') // Si on a un 1 dans l'écriture binaire
+		mpz_mul(result, result, result); // Square (faire le carré)
+		mpz_mod(result, result, n); // Modulo du carré
+		if(mpz_tstbit(h, i) == 1)
 		{
 			mpz_mul(result, result, a); // Multiply
-			mpz_mod(result, result, n);
+			mpz_mod(result, result, n); // Modulo de result*a
 		}
-		gmp_printf("Result = %Zd mod %Zd \n", result, n);
 	}
-	free(e.str);
 }
-
 
 void fermat(mpz_t n, int k) // n est un grand entier et k est juste un int, c'est le nombre de répétition
 {
@@ -77,22 +33,25 @@ void fermat(mpz_t n, int k) // n est un grand entier et k est juste un int, c'es
 	gmp_randinit_default(state);
 	printf("%s\n", "initialisation state ok");
 	
-	mpz_t a, result, temp;
+	mpz_t a, result, tmp, tmp2;
 	printf("%s\n", "déclaration des mpz_t ok");
 	mpz_init(result);
-	mpz_init(temp);
+	mpz_init(tmp);
 	mpz_init(a);
-	mpz_sub_ui(temp, n, 1);
+	mpz_init(tmp2);
+	mpz_sub_ui(tmp, n, 1);
+	mpz_sub_ui(tmp2, n, 3);
 	printf("%s\n", "initilisation des mpz_t ok");
 	
 	int i;
 	for (i = 1; i < k; i++)
 	{
 		printf("%s\n", "entrée en boucle ok");
-		mpz_urandomm(a, state, n);
+		mpz_urandomm(a, state, tmp2); // Attention ici on doit enlever le 0
+		mpz_add_ui(a, a, 2);
 		gmp_printf("%s%Zd\n", "valeur du random créé :", a);
 		
-		square_multiply(a, temp, n, result); // Calcul de l'exponentielle dans a
+		square_multiply(a, tmp, n, result); // Calcul de l'exponentielle dans a
 		printf("%s\n", "calcul de la valeur de l'entier à tester ok");
 		
 		if(mpz_cmp_ui(result, 1) != 0) // On cherche à savoir si le random puissance n-1 modulo n est égal à 1
@@ -112,77 +71,63 @@ mp_bitcnt_t decomposer(mpz_t tmp, mpz_t t) // tmp : nombre à décomposer (n-1),
 	mpz_t div;
 	mpz_init_set_str(div, "0", 10);
 	mp_bitcnt_t s = mpz_scan1(tmp, 0);
-	printf("s1 : %lu\n", s);
-	mpz_setbit(div, s); // Mettre le bit s de div à 1
-	printf("s1,5 : %lu\n", s);
-	mpz_cdiv_q(t, tmp, div); // Trouver t
-	gmp_printf("t : %Zd\n", t);
+	mpz_setbit(div, s); // Mettre le bit s de div à 1 et div = 2^s
+	mpz_cdiv_q(t, tmp, div); // Trouver t = tmp/div
 	mpz_clear(div);
 	return s;
 }
 
 int miller_rabin(mpz_t n, int k)
 {
-	mpz_t a, y, t, tmp;
-	mpz_init(a);
-	mpz_init(y);
-	mpz_init(t);
-	mpz_init(tmp);
+	int i, j, bool = 0;
+	mpz_t r, y, t, tmp, tmp2;
+	mpz_init(r); mpz_init(y); mpz_init(t); mpz_init(tmp); mpz_init(tmp2);
 	
-	int i, j;
-	
-	mpz_sub_ui(tmp, n, 1); // La décomposition se fait sur n-1 stocké dans tmp
-	mp_bitcnt_t s = decomposer(tmp, t); // Appel de la fonction de décomposition de n-1 sous forme (2^s)*t avec t impair
-	printf("s2 : %lu\n", s);
-	gmp_printf("t2 : %Zd\n", t);
 	gmp_randstate_t state; // Création de la graine du random
 	gmp_randinit_default(state); //Initalisation de la graine du random
 	
+	mpz_sub_ui(tmp, n, 1); // La décomposition se fait sur n-1 stocké dans tmp
+	mp_bitcnt_t s = decomposer(tmp, t); // Appel de la fonction de décomposition de n-1 sous forme (2^s)*t avec t impair
+	
+	mpz_sub_ui(tmp2, tmp, 1); // On prend n-2 pour borne supérieur de la génération de l'aléatoire puis on rajout 1 afin de ne pas tomber sur un aléatoire égal à zéro
+	
 	for (i = 1; i < k; i++)
 	{
-		mpz_urandomm(a, state, n); //Génération du random 0<a<n
-		gmp_printf("a : %Zd\n", a);
-		square_multiply(a, n, t, y); //Calcul de a^t mod n
-		if((mpz_cmp_ui(y, 1) != 0) && (mpz_cmp_ui(y, -1) != 0)) // y!=1 et y!=-1
+		mpz_urandomm(r, state, tmp2); //Génération du random 0<r<n
+		mpz_add_ui(r, r, 1);
+		
+		square_multiply(r, n, t, y); //Calcul de y = r^t mod n
+		
+		if((mpz_cmp_ui(y, 1) != 0) && (mpz_cmp(y, tmp) != 0)) // y!=1 et y!=-1
 		{
-			s = s - 1;
-			printf("s3 : %lu\n", s);
-			for (j = 1; j < s; j++)
+			for (j = 0; j < s-1; j++)
 			{
-				printf("s4 : %lu\n", s);
 				mpz_mul(y, y, y); //y=y^2
 				mpz_mod(y, y, n); //y=y mod n;
-				gmp_printf("y : %Zd\n", y);
 				if(mpz_cmp_ui(y, 1) == 0)
 				{
-					printf("%s\n", "Composé1");
-					mpz_clear(a);
-					mpz_clear(y);
-					mpz_clear(t);
-					mpz_clear(tmp);
+					gmp_printf(" %Zd est composé\n", n);
+					mpz_clear(r); mpz_clear(y); mpz_clear(t); mpz_clear(tmp);
 					gmp_randclear(state);
 					return 0;
 				}
-				if(mpz_cmp_ui(y, -1) == 0)
+				if(mpz_cmp(y, tmp) == 0)
 				{
+					bool = 1;
 					break; //Arrêter la boucle j et continuer avec le i suivant
 				}
-				s = s-1;
 			}
-			printf("%s\n", "Composé2");
-			mpz_clear(a);
-			mpz_clear(y);
-			mpz_clear(t);
-			mpz_clear(tmp);
-			gmp_randclear(state);
-			return 0;
+			if(bool == 0)
+			{
+				gmp_printf(" %Zd est composé\n", n);
+				mpz_clear(r); mpz_clear(y); mpz_clear(t); mpz_clear(tmp);
+				gmp_randclear(state);
+				return 0;
+			}
 		}		
 	}
-	printf("%s\n", "Premier");	
-	mpz_clear(a);
-	mpz_clear(y);
-	mpz_clear(t);
-	mpz_clear(tmp);
+	gmp_printf(" %Zd est premier\n", n);
+	mpz_clear(r); mpz_clear(y); mpz_clear(t); mpz_clear(tmp);
 	gmp_randclear(state);
 	return 0;
 }
--- main.c
+++ main.c
@@ -2,22 +2,26 @@
 #include <gmp.h>
 #include "fonction.h"
 
-int main() 
+int main(int argc, char** argv)
 {
-	mpz_t a;// n, h,result;
-	//mpz_init_set_str(h,"12",10);
-	mpz_init_set_str(a,"21",10);
-	//mpz_init_set_str(n,"53",10);
-	//mpz_init(result);
-	//square_multiply(a,n,h,result);
+	mpz_t a;
+	
+	mpz_init_set_str(a, argv[1], 10); // Nombre à tester initié à la valeur saisie en argument d'exécution
+	
+	if(mpz_cmp_ui(a, 3) == -1) 
+	{
+		gmp_printf("Le nombre %Zd est premier\n", a);
+		return 0;
+	}
 	if((mpz_cmp_ui(a, 2) != 0) && (mpz_tstbit(a, 0) == 0)) // Si nombre est pair et différent de 2 alors il est toujours composé
 	{
-		printf("%s\n", "Composé");
+		gmp_printf("Le nombre %Zd est composé\n", a);
 		return 0;
 	}
 	
-	int k = 2;
+	int k = 3;
 	//fermat(a, k); // Test : savoir si a est premier ou pas avec un nombre d'itération de k
+	printf("D'après l'algorithme de Miller-Rabin, le nombre");
 	int ok = miller_rabin(a, k);
 	return ok;
 }
